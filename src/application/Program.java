2089e399ea812b76961a1eb2bee0683503e4164c
Ruan Domingues <ruandm497@gmail.com>
2020-04-28 23:24:40 -0300

    Mensagem acrescentada

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,10 +3,8 @@ package application;
 public class Program {
 	public static void main(String[]args) {
 		
-		int a = 100;
-		String b = (String) a;
 		
-		System.out.println();
+		System.out.println("Teste");
 	}
 
 	
